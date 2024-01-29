b7d99cb989e88f5b86bb05171b8e7db0404e314a
Noah23Mautner <152974783+Noah23Mautner@users.noreply.github.com>
2024-01-29 17:06:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petlje3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite prvi broj:");
+            int prviBroj = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite drugi broj:");
+            int drugiBroj = int.Parse(Console.ReadLine());
+
+            int najveciDjeljiviBroj = 0;
+
+            for (int i = 1; i <= Math.Min(prviBroj, drugiBroj); i++)
+            {
+                if (prviBroj % i == 0 && drugiBroj % i == 0)
+                {
+                    najveciDjeljiviBroj = i;
+                }
+            }
+
+            Console.WriteLine("Najveći broj djeljiv s oba broja je: " + najveciDjeljiviBroj);
+            Console.ReadKey();
+        }
+    }
+}
